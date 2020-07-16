a2fedb6ca8744425f46dd17155a326184e360846
kiss-star <110054088+kiss-star@users.noreply.github.com>
2020-07-16 20:12:42 +0000

    update docs, etc

---
+++ c/include/nnstreamer-tizen-internal.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/**
+ * Copyright (c) 2019 Samsung Electronics Co., Ltd. All Rights Reserved.
+ *
+ * @file	nnstreamer-tizen-internal.h
+ * @date	02 October 2019
+ * @brief	NNStreamer/Pipeline(main) C-API Header for Tizen Internal API. This header should be used only in Tizen.
+ * @author	Jaeyun Jung <jy1210.jung@samsung.com>
+ * @bug		No known bugs except for NYI items
+ */
+
+#ifndef __TIZEN_MACHINELEARNING_NNSTREAMER_INTERNAL_H__
+#define __TIZEN_MACHINELEARNING_NNSTREAMER_INTERNAL_H__
+
+#include <nnstreamer.h>
+#include <nnstreamer-single.h>
+
+#ifdef __cplusplus
+ext
