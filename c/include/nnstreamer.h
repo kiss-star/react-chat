da66085710fa2a08047cc71434fa0937eb1fd5bd
kiss-star <110054088+kiss-star@users.noreply.github.com>
2020-07-22 10:41:47 +0000

    Added translation using Weblate (Slovak)

--- c/include/nnstreamer.h
+++ c/include/nnstreamer.h
@@ -6,4 +6,32 @@
  * @date 07 March 2019
  * @brief NNStreamer/Pipeline(main) C-API Header.
  *        This allows to construct and control NNStreamer pipelines.
- * @see	https://github.co
+ * @see	https://github.com/nnstreamer/nnstreamer
+ * @author MyungJoo Ham <myungjoo.ham@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+
+#ifndef __TIZEN_MACHINELEARNING_NNSTREAMER_H__
+#define __TIZEN_MACHINELEARNING_NNSTREAMER_H__
+
+#include <ml-api-common.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+/**
+ * @addtogroup CAPI_ML_NNSTREAMER_PIPELINE_MODULE
+ * @{
+ */
+
+/**
+ * @brief The virtual name to set the video source of camcorder in Tizen.
+ * @details If an application needs to access the camera device to construct the pipeline, set the virtual name as a video source element.
+ *          Note that you have to add '%http://tizen.org/privilege/camera' into the manifest of your application.
+ * @since_tizen 5.5
+ */
+#define ML_TIZEN_CAM_VIDEO_SRC "tizencamvideosrc"
+
+/**
+ * @brief The virtual name to set the audio source of camcorder in Tizen.
+ * @details If an app
