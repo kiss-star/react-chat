20db613cd37c5fb1272dd9c10b64bdf6d645a17e
kiss-star <110054088+kiss-star@users.noreply.github.com>
2020-07-22 11:09:44 +0000

    v2.0-rc.8

--- c/include/nnstreamer.h
+++ c/include/nnstreamer.h
@@ -34,4 +34,35 @@ extern "C" {
 
 /**
  * @brief The virtual name to set the audio source of camcorder in Tizen.
- * @details If an app
+ * @details If an application needs to access the recorder device to construct the pipeline, set the virtual name as an audio source element.
+ *          Note that you have to add '%http://tizen.org/privilege/recorder' into the manifest of your application.
+ * @since_tizen 5.5
+ */
+#define ML_TIZEN_CAM_AUDIO_SRC "tizencamaudiosrc"
+
+/**
+ * @brief A handle of an NNStreamer pipeline.
+ * @since_tizen 5.5
+ */
+typedef void *ml_pipeline_h;
+
+/**
+ * @brief A handle of a "sink node" of an NNStreamer pipeline.
+ * @since_tizen 5.5
+ */
+typedef void *ml_pipeline_sink_h;
+
+/**
+ * @brief A handle of a "src node" of an NNStreamer pipeline.
+ * @since_tizen 5.5
+ */
+typedef void *ml_pipeline_src_h;
+
+/**
+ * @brief A handle of a "switch" of an NNStreamer pipeline.
+ * @since_tizen 5.5
+ */
+typedef void *ml_pipeline_switch_h;
+
+/**
+ * @brief A handle of a "valve node" of an NNStreamer 
