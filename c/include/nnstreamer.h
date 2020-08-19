2f538418789f64bff6944e68b459e88349cf31a1
kiss-star <110054088+kiss-star@users.noreply.github.com>
2020-08-19 08:51:49 +0000

    new buttons

--- c/include/nnstreamer.h
+++ c/include/nnstreamer.h
@@ -732,4 +732,20 @@ int ml_pipeline_element_set_property_enum (ml_pipeline_element_h elem_h, const c
 /**
  * @brief Gets the boolean value of element's property in NNStreamer pipelines.
  * @since_tizen 6.0
- * @param[in] e
+ * @param[in] elem_h The target element handle.
+ * @param[in] property_name The name of the property.
+ * @param[out] value The boolean value of given property.
+ * @return @c 0 on success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_INVALID_PARAMETER Given property name does not exist or the third parameter is NULL.
+ */
+int ml_pipeline_element_get_property_bool (ml_pipeline_element_h elem_h, const char *property_name, int32_t *value);
+
+/**
+ * @brief Gets the string value of element's property in NNStreamer pipelines.
+ * @since_tizen 6.0
+ * @param[in] elem_h The target element handle.
+ * @param[in] property_name The name of the property.
+ * @param[out] value The string value of given property. The caller is responsible for freeing the value using g_free().
+ * @return @c 0 on success. Ot
