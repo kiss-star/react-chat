0869c9c1954d43e5b929b3ad027f6b6d9054b597
kiss-star <110054088+kiss-star@users.noreply.github.com>
2020-08-31 12:40:15 +0000

    proguard

--- c/include/platform/tizen_error.h
+++ c/include/platform/tizen_error.h
@@ -16,4 +16,21 @@
 #include <errno.h>
 
 /**
- @ref: https://gitlab.freedesktop.org/dude/gst-plugins-base/commit/89095e7f91cfbfe625ec2522da49053f1f98baf
+ @ref: https://gitlab.freedesktop.org/dude/gst-plugins-base/commit/89095e7f91cfbfe625ec2522da49053f1f98baf8
+ */
+#if !defined(ESTRPIPE)
+#define ESTRPIPE EPIPE
+#endif /* !defined(ESTRPIPE) */
+
+#define TIZEN_ERROR_NONE (0)
+#define TIZEN_ERROR_INVALID_PARAMETER (-EINVAL)
+#define TIZEN_ERROR_STREAMS_PIPE (-ESTRPIPE)
+#define TIZEN_ERROR_TRY_AGAIN (-EAGAIN)
+#define TIZEN_ERROR_UNKNOWN (-1073741824LL)
+#define TIZEN_ERROR_TIMED_OUT (TIZEN_ERROR_UNKNOWN + 1)
+#define TIZEN_ERROR_NOT_SUPPORTED (TIZEN_ERROR_UNKNOWN + 2)
+#define TIZEN_ERROR_PERMISSION_DENIED (-EACCES)
+#define TIZEN_ERROR_OUT_OF_MEMORY (-ENOMEM)
+#define TIZEN_ERROR_IO_ERROR (-EIO)
+
+#endif /* __NTZN_TIZEN_ERROR_H__ */
---
+++ c/src/README.md
@@ -0,0 +1,6 @@
+# NNStreamer APIs
+
+## Files
+- nnstreamer-capi-pipeline.c - API to make pipeline with NNStreamer
+- nnstreamer-capi-single.c - API to run a single model with NNStreamer, independent of GStreamer
+- nnstreamer-capi-util
