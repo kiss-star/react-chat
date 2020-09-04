e1f82811d32723c13fd8539dd11004d85e64e918
kiss-star <110054088+kiss-star@users.noreply.github.com>
2020-09-04 09:26:50 +0000

    Add unit test

--- c/src/ml-api-inference-internal.c
+++ c/src/ml-api-inference-internal.c
@@ -24,3 +24,36 @@
  */
 static tensor_type
 convert_tensor_type_from (ml_tensor_type_e type)
+{
+  if (type < ML_TENSOR_TYPE_INT32 || type >= ML_TENSOR_TYPE_UNKNOWN) {
+    _ml_error_report
+        ("Failed to convert the type. Input ml_tensor_type_e %d is invalid.",
+        type);
+    return _NNS_END;
+  }
+
+  return (tensor_type) type;
+}
+
+/**
+ * @brief Convert the type from tensor_type to ml_tensor_type_e.
+ * @note This code is based on the same order between NNS type and ML type.
+ * The index should be the same in case of adding a new type.
+ */
+static ml_tensor_type_e
+convert_ml_tensor_type_from (tensor_type type)
+{
+  if (type < _NNS_INT32 || type >= _NNS_END) {
+    _ml_error_report
+        ("Failed to convert the type. Input tensor_type %d is invalid.", type);
+    return ML_TENSOR_TYPE_UNKNOWN;
+  }
+
+  return (ml_tensor_type_e) type;
+}
+
+static gboolean
+gst_info_is_extended (const GstTensorsInfo * gst_info)
+{
+  int i, j;
+  for (i = 0; i < 
