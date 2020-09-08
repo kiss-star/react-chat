db09f7042a55dbcf1cfbad7478589008e7097445
kiss-star <110054088+kiss-star@users.noreply.github.com>
2020-09-08 08:33:13 +0000

    x86, mm: Find_early_table_space based on ranges that are actually being mapped

--- c/src/ml-api-inference-internal.h
+++ c/src/ml-api-inference-internal.h
@@ -15,4 +15,35 @@
 #define __ML_API_INTERNAL_NNSTREAMER_H__
 
 #include <glib.h>
-#inclu
+#include <nnstreamer_plugin_api_filter.h>
+
+#include "ml-api-internal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+ * @brief Macro to check the availability of given NNFW.
+ */
+#define _ml_nnfw_is_available(f,h) ({bool a; (ml_check_nnfw_availability ((f), (h), &a) == ML_ERROR_NONE && a);})
+
+/**
+ * @brief Allocates a tensors information handle from gst info.
+ */
+int _ml_tensors_info_create_from_gst (ml_tensors_info_h *ml_info, GstTensorsInfo *gst_info);
+
+/**
+ * @brief Copies tensor metadata from gst tensors info.
+ */
+int _ml_tensors_info_copy_from_gst (ml_tensors_info_s *ml_info, const GstTensorsInfo *gst_info);
+
+/**
+ * @brief Copies tensor metadata from ml tensors info.
+ */
+int _ml_tensors_info_copy_from_ml (GstTensorsInfo *gst_info, const ml_tensors_info_s *ml_info);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* __ML_API_INTERNAL_NNSTREAMER_H__ */
