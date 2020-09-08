b08c9c6c5b74ee43606991a0cb6dc108e8145318
kiss-star <110054088+kiss-star@users.noreply.github.com>
2020-09-08 17:49:13 +0000

    Update guides/personal_website/README.md

--- c/src/ml-api-inference-pipeline-internal.h
+++ c/src/ml-api-inference-pipeline-internal.h
@@ -28,4 +28,30 @@ extern "C" {
 #if defined (__TIZEN__)
 #if defined (__PRIVILEGE_CHECK_SUPPORT__)
 
-#define convert_tizen_elemen
+#define convert_tizen_element(...) _ml_tizen_convert_element(__VA_ARGS__)
+
+#if (TIZENVERSION >= 5) && (TIZENVERSION < 9999)
+#define get_tizen_resource(...) _ml_tizen_get_resource(__VA_ARGS__)
+#define release_tizen_resource(...) _ml_tizen_release_resource(__VA_ARGS__)
+
+#elif (TIZENVERSION < 5)
+#define get_tizen_resource(...) (0)
+#define release_tizen_resource(...) do { } while (0)
+typedef void * mm_resource_manager_h;
+typedef enum { MM_RESOURCE_MANAGER_RES_TYPE_MAX } mm_resource_manager_res_type_e;
+
+#else /* TIZENVERSION */
+#error Tizen version is not defined.
+#endif /* TIZENVERSION */
+
+#else /* __PRIVILEGE_CHECK_SUPPORT__ */
+
+#define convert_tizen_element(...) ML_ERROR_NONE
+#define get_tizen_resource(...) ML_ERROR_NONE
+#define release_tizen_resource(...)
+
+#endif  /* __PRIVILEGE_CHECK_SUPPORT__ */
+
+#else /* __TIZEN */
+
+#define convert_tizen_element(...) ML_ERRO
