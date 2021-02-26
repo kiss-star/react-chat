c5b5e92734d0add9ba0826454c3f87ed4b6a5767
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-02-26 22:04:08 +0000

    Remove config

--- daemon/includes/service-db.hh
+++ daemon/includes/service-db.hh
@@ -5,4 +5,32 @@
  * @file service-db.hh
  * @date 28 Mar 2022
  * @brief NNStreamer/Service Database Interface
- * @see	https://github.com/nnstrea
+ * @see	https://github.com/nnstreamer/api
+ * @author Sangjung Woo <sangjung.woo@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+
+#ifndef __SERVICE_DB_HH__
+#define __SERVICE_DB_HH__
+
+#include <sqlite3.h>
+#include <iostream>
+
+/**
+ * @brief Class for ML-Service Database.
+ */
+class MLServiceDB
+{
+public:
+  MLServiceDB (const MLServiceDB &) = delete;
+  MLServiceDB (MLServiceDB &&) = delete;
+  MLServiceDB & operator= (const MLServiceDB &) = delete;
+  MLServiceDB & operator= (MLServiceDB &&) = delete;
+
+  virtual void connectDB ();
+  virtual void disconnectDB ();
+  virtual void set_pipeline (const std::string name, const std::string description);
+  virtual void get_pipeline (const std::string name, std::string &description);
+  virtual void delete_pipeline (const std::string name);
+  /** @todo update methods to handle nn model */
+  virtual void set_model (con
