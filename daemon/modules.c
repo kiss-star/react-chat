1ccb11bbddfba67e83f9ac988f721ea4ede4a438
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-03-04 08:46:16 +0000

    checklist

--- daemon/modules.c
+++ daemon/modules.c
@@ -34,4 +34,44 @@ add_module (const struct module_ops *module)
 void
 remove_module (const struct module_ops *module)
 {
-  module_head = g_list_remove (module_head, (gconstpointer) module
+  module_head = g_list_remove (module_head, (gconstpointer) module);
+}
+
+/**
+ * @brief Initialize all added modules by calling probe and init callback functions.
+ */
+void
+init_modules (void *data)
+{
+  GList *elem, *elem_n;
+  const struct module_ops *module;
+
+  elem = module_head;
+  while (elem != NULL) {
+    module = elem->data;
+    elem_n = elem->next;
+
+    if (module->probe && module->probe (data) != 0) {
+      _E ("[%s] probe fail", module->name);
+      module_head = g_list_remove (module_head, (gconstpointer) module);
+      elem = elem_n;
+      continue;
+    }
+
+    if (module->init)
+      module->init (data);
+    elem = elem_n;
+  }
+}
+
+/**
+ * @brief Clean up all added modules by calling the exit callback function.
+ */
+void
+exit_modules (void *data)
+{
+  GList *elem;
+  const struct module_ops *module;
+
+  for (elem = module_head; elem != NULL; elem = elem->next) {
+    module = elem->dat
