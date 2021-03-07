f640e0e8e7d26613f7089ce7a5cb4861908a9ae5
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-03-07 15:39:58 +0000

    circle

--- daemon/pipeline-dbus-impl.cc
+++ daemon/pipeline-dbus-impl.cc
@@ -30,4 +30,51 @@
 #include "service-db.hh"
 #include "dbus-interface.h"
 
-static MachinelearningServicePipeline 
+static MachinelearningServicePipeline *g_gdbus_instance = NULL;
+static GHashTable *pipeline_table = NULL;
+G_LOCK_DEFINE_STATIC (pipeline_table_lock);
+
+/**
+ * @brief Structure for pipeline.
+ */
+typedef struct _pipeline
+{
+  GstElement *element;
+  gint64 id;
+  GMutex lock;
+  gchar *service_name;
+  gchar *description;
+} pipeline_s;
+
+/**
+ * @brief Internal function to destroy pipeline instances.
+ */
+static void
+_pipeline_free (gpointer data)
+{
+  pipeline_s *p;
+
+  if (!data) {
+    _E ("internal error, the data should not be NULL");
+    return;
+  }
+
+  p = (pipeline_s *) data;
+
+  if (p->element)
+    gst_object_unref (p->element);
+
+  g_free (p->service_name);
+  g_free (p->description);
+  g_mutex_clear (&p->lock);
+
+  g_free (p);
+}
+
+/**
+ * @brief Get the skeleton object of the DBus interface.
+ */
+static MachinelearningServicePipeline *
+gdbus_get_pipeline_instance (void)
+{
+  return machinelearning_service_pipeline_sk
