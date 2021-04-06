860b6048d97ad5c2be48594d175bad76c4459cba
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-04-06 14:53:29 +0000

    version 1.1.5

--- java/android/nnstreamer/src/main/java/org/nnsuite/nnstreamer/Pipeline.java
+++ java/android/nnstreamer/src/main/java/org/nnsuite/nnstreamer/Pipeline.java
@@ -16,4 +16,19 @@ import java.util.HashMap;
  * - Create a stream pipeline with NNStreamer plugins, GStreamer plugins.<br>
  * - Interfaces to push data to the pipeline from the application.<br>
  * - Interfaces to pull data from the pipeline to the application.<br>
- * - Interfaces to start/stop/destroy the pipel
+ * - Interfaces to start/stop/destroy the pipeline.<br>
+ * - Interfaces to control switches and valves in the pipeline.<br>
+ */
+public final class Pipeline implements AutoCloseable {
+    private long mHandle = 0;
+    private HashMap<String, ArrayList<NewDataCallback>> mSinkCallbacks = new HashMap<>();
+    private StateChangeCallback mStateCallback = null;
+
+    private static native boolean nativeCheckElementAvailability(String element);
+    private native long nativeConstruct(String description, boolean addStateCb);
+    private native void nativeDestroy(long handle);
+    private native boolean nativeStart(long handle);
+    private native boolean nativeStop(long handle);
+    private native boolean nativeFlush(long handle, boolean start);
+    private native int nativeGetState(long handle);
+    private native boolean nativeInputData(long handle, String
