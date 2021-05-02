d94650636c686778b51c8daddd3790de455fa784
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-05-02 15:04:26 +0000

    Travis

--- java/android/nnstreamer/src/main/java/org/nnsuite/nnstreamer/TensorsInfo.java
+++ java/android/nnstreamer/src/main/java/org/nnsuite/nnstreamer/TensorsInfo.java
@@ -4,4 +4,36 @@
  * Copyright (C) 2019 Samsung Electronics Co., Ltd.
  */
 
-package org.nnsuite.nnstreamer
+package org.nnsuite.nnstreamer;
+
+import java.util.ArrayList;
+
+/**
+ * Provides interfaces to handle tensors information.
+ *
+ * @see NNStreamer#TENSOR_RANK_LIMIT
+ * @see NNStreamer#TENSOR_SIZE_LIMIT
+ * @see NNStreamer.TensorType
+ */
+public final class TensorsInfo implements AutoCloseable, Cloneable {
+    private ArrayList<TensorInfo> mInfoList = new ArrayList<>();
+
+    /**
+     * Allocates a new {@link TensorsData} instance with the tensors information.
+     *
+     * @return {@link TensorsData} instance
+     *
+     * @throws IllegalStateException if tensors info is empty
+     */
+    public TensorsData allocate() {
+        if (getTensorsCount() == 0) {
+            throw new IllegalStateException("Empty tensor info");
+        }
+
+        return TensorsData.allocate(this);
+    }
+
+    /**
+     * Creates a new {@link TensorsInfo} instance cloned from the current tensors information.
+     *
+     * @return {@link Tens
