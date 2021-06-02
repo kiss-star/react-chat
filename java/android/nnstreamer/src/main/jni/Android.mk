c5c532f66f6b4c464d19c9f6d4ac2afeb92b9f6d
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-06-02 09:54:47 +0000

    Delete lib

--- java/android/nnstreamer/src/main/jni/Android.mk
+++ java/android/nnstreamer/src/main/jni/Android.mk
@@ -21,4 +21,41 @@ GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/x86
 else ifeq ($(TARGET_ARCH_ABI),x86_64)
 GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/x86_64
 else
-$(error Target arch ABI not suppo
+$(error Target arch ABI not supported: $(TARGET_ARCH_ABI))
+endif
+
+# Set ML API Version
+ML_API_VERSION  := 1.8.3
+ML_API_VERSION_MAJOR := $(word 1,$(subst ., ,${ML_API_VERSION}))
+ML_API_VERSION_MINOR := $(word 2,$(subst ., ,${ML_API_VERSION}))
+ML_API_VERSION_MICRO := $(word 3,$(subst ., ,${ML_API_VERSION}))
+
+#------------------------------------------------------
+# API build option
+#------------------------------------------------------
+include $(NNSTREAMER_ROOT)/jni/nnstreamer.mk
+
+NNSTREAMER_API_OPTION := all
+
+# tensor-query support
+ENABLE_TENSOR_QUERY := true
+
+# tensorflow-lite (nnstreamer tf-lite subplugin)
+ENABLE_TF_LITE := false
+
+# SNAP (Samsung Neural Acceleration Platform)
+ENABLE_SNAP := false
+
+# NNFW (On-device neural network inference framework, Samsung Research)
+ENABLE_NNFW := false
+
+# SNPE (Snapdragon Neural Processing Engine)
+ENABLE_SNPE := false
+
+# PyTorch
+ENABLE_PYTORCH := false
+
+# MXNet
+ENABLE_MXNET := false
+
+# C
