7dad71ec272bc9d48896243a9eb582340a467f3f
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-06-15 15:22:13 +0000

    Pickup child project changes

--- packaging/run_unittests.sh
+++ packaging/run_unittests.sh
@@ -11,4 +11,25 @@ pushd build
 
 run_entry() {
   entry=$1
-  ${e
+  ${entry} --gtest_output="xml:${entry##*/}.xml"
+  return $?
+}
+
+if [ -f "$1" ]; then
+    echo $1
+    run_entry $1
+    exit $?
+elif [ -d "$1" ]; then
+    testlist=$(find $1 -type f -executable -name "unittest_*")
+    for t in ${testlist}; do
+	echo "running: ${t} @$(pwd)"
+	run_entry ${t}
+	ret=$?
+	if [ $ret -ne 0 ]; then
+	    break
+	fi
+    done
+fi
+
+popd
+exit $ret
