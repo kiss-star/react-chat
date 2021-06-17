6414e4ff2368c19063fda8ffd6dbcbaf86acd5ae
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-06-17 17:18:12 +0000

    Update bootstrap.R

---
+++ tests/capi/unittest_capi_inference.cc
@@ -0,0 +1,13 @@
+/**
+ * @file        unittest_capi_inference.cc
+ * @date        13 Mar 2019
+ * @brief       Unit test for ML inference C-API.
+ * @see         https://github.com/nnstreamer/nnstreamer
+ * @author      MyungJoo Ham <myungjoo.ham@samsung.com>
+ * @bug         No known bugs
+ */
+
+#include <gtest/gtest.h>
+#include <glib.h>
+#include <glib/gstdio.h> /* GStatBuf */
+#include
