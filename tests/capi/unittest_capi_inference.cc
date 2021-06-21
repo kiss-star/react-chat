b3b3b77ac7108b02056ef55ad66d0ee82e4ec617
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-06-21 15:56:51 +0000

    bump version code

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -133,3 +133,37 @@ TEST (nnstreamer_capi_construct_destruct, tizen_internal_01_p)
  * @brief Test NNStreamer pipeline construct with Tizen internal API.
  */
 TEST (nnstreamer_capi_construct_destruct, tizen_internal_02_p)
+{
+  ml_pipeline_h handle;
+  gchar *pipeline;
+  int status;
+
+  pipeline = g_strdup_printf (
+      "audiotestsrc ! audioconvert ! audio/x-raw,format=S16LE,rate=16000 ! tensor_converter ! tensor_sink");
+
+  status = ml_pipeline_construct_internal (pipeline, NULL, NULL, &handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_destroy (handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  g_free (pipeline);
+}
+#endif /* __TIZEN__ */
+
+/**
+ * @brief Test NNStreamer pipeline construct & destruct
+ */
+TEST (nnstreamer_capi_construct_destruct, dummy_01)
+{
+  const char *pipeline = "videotestsrc num_buffers=2 ! fakesink";
+  ml_pipeline_h handle;
+  int status = ml_pipeline_construct (pipeline, NULL, NULL, &handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_destroy (handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+}
+
+/**
+ * @brief Test
