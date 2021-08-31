e69c8f9f1f2601ad79367f8c1189f1c00797db44
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-08-31 07:37:30 +0000

    testing auth

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -1788,4 +1788,29 @@ TEST (nnstreamer_capi_switch, failure_01_n)
   ml_pipeline_switch_e type;
   int status;
 
-  /* invalid p
+  /* invalid param : pipe */
+  status = ml_pipeline_switch_get_handle (NULL, "ins", &type, &switchhandle);
+  EXPECT_EQ (status, ML_ERROR_INVALID_PARAMETER);
+}
+
+/**
+ * @brief Test NNStreamer pipeline switch
+ * @detail Failure case to handle input-selector element with invalid param.
+ */
+TEST (nnstreamer_capi_switch, failure_02_n)
+{
+  ml_pipeline_h handle;
+  ml_pipeline_switch_h switchhandle;
+  ml_pipeline_switch_e type;
+  gchar *pipeline;
+  int status;
+
+  pipeline = g_strdup ("input-selector name=ins ! tensor_converter ! tensor_sink name=sinkx "
+                       "videotestsrc is-live=true ! videoconvert ! ins.sink_0 "
+                       "videotestsrc num-buffers=3 ! videoconvert ! ins.sink_1");
+
+  status = ml_pipeline_construct (pipeline, NULL, NULL, &handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  /* invalid param : name */
+  status = ml_pipeline_sw
