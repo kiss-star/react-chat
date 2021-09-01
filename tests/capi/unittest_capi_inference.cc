21e85af1a54bf9c5c3f345a7e9cc159b236f30ba
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-09-01 11:37:52 +0000

    nbd: fix possible memory leak

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -1867,4 +1867,30 @@ TEST (nnstreamer_capi_switch, failure_04_n)
                        "videotestsrc is-live=true ! videoconvert ! ins.sink_0 "
                        "videotestsrc num-buffers=3 ! videoconvert ! ins.sink_1");
 
-  status = ml_pipeline_construct (p
+  status = ml_pipeline_construct (pipeline, NULL, NULL, &handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  /* invalid param : invalid type */
+  status = ml_pipeline_switch_get_handle (handle, "sinkx", &type, &switchhandle);
+  EXPECT_EQ (status, ML_ERROR_INVALID_PARAMETER);
+
+  status = ml_pipeline_destroy (handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test NNStreamer pipeline switch
+ * @detail Failure case to handle input-selector element with invalid param.
+ */
+TEST (nnstreamer_capi_switch, failure_05_n)
+{
+  ml_pipeline_h handle;
+  ml_pipeline_switch_e type;
+  gchar *pipeline;
+  int status;
+
+  pipeline = g_strdup ("input-selector name=ins ! tensor_converter ! tensor_sink name=sinkx "
+                       "videotestsrc is-live=true ! videoconvert ! ins.sink_0 "
+                       "videotestsrc num-buffers=3 ! vid
