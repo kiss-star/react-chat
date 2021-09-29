810ca879223198154674a4523063ad4bcceea1ef
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-09-29 14:28:09 +0000

    net: mvneta: fix operation in 10 Mbit/s mode

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -2327,4 +2327,35 @@ TEST (nnstreamer_capi_util, availability_fail_05_n)
   EXPECT_EQ (status, ML_ERROR_NONE);
   EXPECT_EQ (result, false);
 
-  status = ml_chec
+  status = ml_check_nnfw_availability (ML_NNFW_TYPE_MVNC,
+      ML_NNFW_HW_GPU, &result);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+  EXPECT_EQ (result, false);
+}
+#endif /* ENABLE_MOVIDIUS_NCSDK2 */
+
+#ifdef ENABLE_ARMNN
+/**
+ * @brief Test NNStreamer Utility for checking availability of ARMNN
+ */
+TEST (nnstreamer_capi_util, availability_06)
+{
+  bool result;
+  int status;
+
+  status = ml_check_nnfw_availability (ML_NNFW_TYPE_ARMNN,
+      ML_NNFW_HW_ANY, &result);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+  EXPECT_EQ (result, true);
+
+  status = ml_check_nnfw_availability (ML_NNFW_TYPE_ARMNN,
+      ML_NNFW_HW_AUTO, &result);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+  EXPECT_EQ (result, true);
+
+  status = ml_check_nnfw_availability (ML_NNFW_TYPE_ARMNN,
+      ML_NNFW_HW_CPU, &result);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+  EXPECT_EQ (result, true);
+
+  status = ml_check_nnfw_availa
