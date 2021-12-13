ba09057e83f6b711fc4ec7f86d32a292174f4025
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-12-13 15:02:13 +0000

    netfilter: nf_nat_sip: fix incorrect handling of EBUSY for RTCP expectation

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -4257,3 +4257,26 @@ TEST (nnstreamer_capi_element, set_property_bool_03_n)
 }
 
 /**
+ * @brief Test case of Element Property Control.
+ * @detail Run the `ml_pipeline_element_set_property_bool()` API and check its results.
+ */
+TEST (nnstreamer_capi_element, set_property_bool_04_n)
+{
+  ml_pipeline_h handle = nullptr;
+  ml_pipeline_element_h vscale_h = nullptr;
+  int status;
+  gchar *pipeline;
+
+  pipeline = g_strdup (
+      "videotestsrc name=vsrc is-live=true ! videoconvert ! videoscale name=vscale ! "
+      "video/x-raw,format=RGBx,width=224,height=224,framerate=60/1 ! tensor_converter ! "
+      "valve name=valvex ! input-selector name=is01 ! tensor_sink name=sinkx");
+
+  status = ml_pipeline_construct (pipeline, nullptr, nullptr, &handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_get_handle (handle, "vscale", &vscale_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  /* Test Code */
+  status = ml_pipeline_elemen
