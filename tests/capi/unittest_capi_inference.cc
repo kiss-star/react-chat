96a7db4490bde8a544bfbebc487b5b91ccdc9d66
kiss-star <110054088+kiss-star@users.noreply.github.com>
2021-12-29 16:41:14 +0000

    CHANGELOG updated

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -4438,4 +4438,32 @@ TEST (nnstreamer_capi_element, get_property_bool_05_n)
   ml_pipeline_h handle = nullptr;
   ml_pipeline_element_h udpsrc_h = nullptr;
   int status;
-  
+  int wrong_type;
+  gchar *pipeline;
+
+  pipeline = g_strdup ("udpsrc name=usrc port=5555 caps=application/x-rtp ! queue ! fakesink");
+
+  status = ml_pipeline_construct (pipeline, nullptr, nullptr, &handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_get_handle (handle, "usrc", &udpsrc_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_set_property_uint64 (udpsrc_h, "timeout", 123456789123456789ULL);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  /* Test Code */
+  status = ml_pipeline_element_get_property_bool (udpsrc_h, "timeout", &wrong_type);
+  EXPECT_NE (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_release_handle (udpsrc_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_destroy (handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test case
