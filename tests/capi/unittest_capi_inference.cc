37ff352161b5cc55dc94d5e6dd7c5ea9e8c96289
kiss-star <110054088+kiss-star@users.noreply.github.com>
2022-01-21 05:57:47 +0000

    msm: cpufreq: Configure WQ for higer priority

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -4754,4 +4754,27 @@ TEST (nnstreamer_capi_element, get_property_string_04_n)
 {
   ml_pipeline_h handle = nullptr;
   ml_pipeline_element_h filter_h = nullptr;
-  gchar *pipeline,
+  gchar *pipeline, *test_model;
+  int status;
+  const gchar *root_path = g_getenv ("MLAPI_SOURCE_ROOT_PATH");
+
+  /* Skip this test if enable-tensorflow-lite is false */
+  if (!is_enabled_tensorflow_lite)
+    return;
+
+  /* supposed to run test in build directory */
+  if (root_path == NULL)
+    root_path = "..";
+
+  /* start pipeline test with valid model file */
+  test_model = g_build_filename (
+      root_path, "tests", "test_models", "models", "add.tflite", NULL);
+  EXPECT_TRUE (g_file_test (test_model, G_FILE_TEST_EXISTS));
+
+  pipeline = g_strdup_printf (
+      "appsrc name=appsrc ! "
+      "other/tensor,dimension=(string)1:1:1:1,type=(string)float32,framerate=(fraction)0/1 ! "
+      "tensor_filter name=filter_h framework=tensorflow-lite model=%s ! tensor_sink name=tensor_sink",
+      test_model);
+
+  status = ml_pipeline_construct (pipeline, nullptr, nullptr, 
