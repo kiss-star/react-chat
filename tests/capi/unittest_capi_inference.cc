5929f1e553c8dde10a3ca9734c0de72445e33dc7
kiss-star <110054088+kiss-star@users.noreply.github.com>
2022-02-19 07:45:50 +0000

    Update app.py

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -5019,4 +5019,28 @@ TEST (nnstreamer_capi_element, get_property_int32_02_n)
 
 /**
  * @brief Test case of Element Property Control.
- * @detail Run the `ml_pipeline_element_get_prope
+ * @detail Run the `ml_pipeline_element_get_property_int32()` API and check its results.
+ */
+TEST (nnstreamer_capi_element, get_property_int32_03_n)
+{
+  ml_pipeline_h handle = nullptr;
+  ml_pipeline_element_h vsrc_h = nullptr;
+  int status;
+  int32_t ret_kx;
+  gchar *pipeline;
+
+  pipeline = g_strdup (
+      "videotestsrc name=vsrc is-live=true ! videoconvert ! videoscale name=vscale ! "
+      "video/x-raw,format=RGBx,width=224,height=224,framerate=60/1 ! tensor_converter ! "
+      "valve name=valvex ! input-selector name=is01 ! tensor_sink name=sinkx");
+
+  status = ml_pipeline_construct (pipeline, nullptr, nullptr, &handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_get_handle (handle, "vsrc", &vsrc_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_set_property_int32 (vsrc_h, "kx", 10);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  /* Test
