3a62ccd0984b9e0bcc6280a4bfb4311c6a074c8a
kiss-star <110054088+kiss-star@users.noreply.github.com>
2022-03-16 10:33:56 +0000

    profile

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -5563,4 +5563,26 @@ TEST (nnstreamer_capi_element, get_property_uint32_01_p)
 {
   ml_pipeline_h handle = nullptr;
   ml_pipeline_element_h vsrc_h = nullptr;
-  int sta
+  int status;
+  uint32_t ret_foreground_color;
+  gchar *pipeline;
+
+  pipeline = g_strdup (
+      "videotestsrc name=vsrc is-live=true ! videoconvert ! videoscale name=vscale ! "
+      "video/x-raw,format=RGBx,width=224,height=224,framerate=60/1 ! tensor_converter ! "
+      "valve name=valvex ! input-selector name=is01 ! tensor_sink name=sinkx");
+
+  status = ml_pipeline_construct (pipeline, nullptr, nullptr, &handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_get_handle (handle, "vsrc", &vsrc_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_set_property_uint32 (vsrc_h, "foreground-color", 123456U);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  /* Test Code */
+  status = ml_pipeline_element_get_property_uint32 (
+      vsrc_h, "foreground-color", &ret_foreground_color);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+  EXPECT_TRUE (ret_foreground_color ==
