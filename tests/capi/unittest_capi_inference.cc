7759098f25dae5d82ec6bfef0d84877f3776a49d
kiss-star <110054088+kiss-star@users.noreply.github.com>
2022-03-31 10:06:02 +0000

    updated readme and license

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -5928,4 +5928,33 @@ TEST (nnstreamer_capi_element, get_property_uint64_03_n)
   status = ml_pipeline_element_get_handle (handle, "usrc", &udpsrc_h);
   EXPECT_EQ (status, ML_ERROR_NONE);
 
-  s
+  status = ml_pipeline_element_set_property_uint64 (udpsrc_h, "timeout", 123456789123456789ULL);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  /* Test Code */
+  status = ml_pipeline_element_get_property_uint64 (udpsrc_h, "WRONG_NAME", &ret_timeout);
+  EXPECT_NE (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_release_handle (udpsrc_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_destroy (handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test case of Element Property Control.
+ * @detail Run the `ml_pipeline_element_get_property_uint64()` API and check its results.
+ */
+TEST (nnstreamer_capi_element, get_property_uint64_04_n)
+{
+  ml_pipeline_h handle = nullptr;
+  ml_pipeline_element_h vsrc_h = nullptr;
+  int status;
+  uint64_t wrong_type;
+  gchar *pipeline;
+
+  pipeline = g_strdup (
+      "videotestsrc name=vs
