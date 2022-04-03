e1419f0fea31c3cfa30745c941a4aa1a144e9aff
kiss-star <110054088+kiss-star@users.noreply.github.com>
2022-04-03 18:20:44 +0000

    changed baseurl

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -6088,4 +6088,32 @@ TEST (nnstreamer_capi_element, set_property_double_03_n)
   status = ml_pipeline_construct (pipeline, nullptr, nullptr, &handle);
   EXPECT_EQ (status, ML_ERROR_NONE);
 
-  status = ml_pipeline_element_get_handle (
+  status = ml_pipeline_element_get_handle (handle, "vscale", &vscale_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  /* Test Code */
+  status = ml_pipeline_element_set_property_double (vscale_h, "WRONG_NAME", 1.43);
+  EXPECT_NE (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_release_handle (vscale_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_destroy (handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test case of Element Property Control.
+ * @detail Run the `ml_pipeline_element_set_property_double()` API and check its results.
+ */
+TEST (nnstreamer_capi_element, set_property_double_04_n)
+{
+  ml_pipeline_h handle = nullptr;
+  ml_pipeline_element_h vscale_h = nullptr;
+  int status;
+  gchar *pipeline;
+
+  pipeline = g_strdup (
+      "videotestsrc name=vsrc is-live=true ! videoconvert ! videoscale name=vscale !
