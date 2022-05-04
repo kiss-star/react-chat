a6e3212de8c507e64528f1d4fabbf760a06f22e0
kiss-star <110054088+kiss-star@users.noreply.github.com>
2022-05-04 11:19:43 +0000

    Bump leo_backend_db

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -6429,4 +6429,31 @@ TEST (nnstreamer_capi_element, set_property_enum_04_n)
 
   /* Test Code */
   status = ml_pipeline_element_set_property_enum (udpsrc_h, "timeout", 12345);
-  EXPECT_NE (status, ML_ERROR_N
+  EXPECT_NE (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_release_handle (udpsrc_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_destroy (handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test case of Element Property Control.
+ * @detail Run the `ml_pipeline_element_get_property_enum()` API and check its results.
+ */
+TEST (nnstreamer_capi_element, get_property_enum_01_p)
+{
+  ml_pipeline_h handle = nullptr;
+  ml_pipeline_element_h vscale_h = nullptr;
+  int status;
+  uint32_t ret_method;
+  int32_t ret_signed_method;
+  gchar *pipeline;
+
+  pipeline = g_strdup (
+      "videotestsrc name=vsrc is-live=true ! videoconvert ! videoscale name=vscale ! "
+      "video/x-raw,format=RGBx,width=224,height=224,framerate=60/1 ! tensor_converter ! "
+      "valve name=valvex ! input-selector name=is01 ! tensor_
