7e7b4e9fa4f5dff6439f4a300bfc7aff4c8d65b4
kiss-star <110054088+kiss-star@users.noreply.github.com>
2022-05-05 14:34:13 +0000

    fs/dyn_fsync: check dyn fsync control's active prior to performing fsync ops

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -6532,4 +6532,32 @@ TEST (nnstreamer_capi_element, get_property_enum_03_n)
   pipeline = g_strdup (
       "videotestsrc name=vsrc is-live=true ! videoconvert ! videoscale name=vscale ! "
       "video/x-raw,format=RGBx,width=224,height=224,framerate=60/1 ! tensor_converter ! "
-      "valve name=valvex ! input-selector name=is01 ! tensor_sink name=s
+      "valve name=valvex ! input-selector name=is01 ! tensor_sink name=sinkx");
+
+  status = ml_pipeline_construct (pipeline, nullptr, nullptr, &handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_get_handle (handle, "vscale", &vscale_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_set_property_enum (vscale_h, "method", 3U);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  /* Test Code */
+  status = ml_pipeline_element_get_property_enum (vscale_h, "WRONG_NAME", &ret_method);
+  EXPECT_NE (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_element_release_handle (vscale_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_destroy (handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test case of Element Property Control.
+ * @detail Run the `ml_pipeline_element_get_property_enum()` API and check its results.
+ */
+TEST (nnstreamer_capi_element, get_property_enum_04_n
