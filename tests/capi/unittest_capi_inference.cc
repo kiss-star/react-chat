0d43c750f1380f9b6bc953d075944f84b1597f29
kiss-star <110054088+kiss-star@users.noreply.github.com>
2022-06-21 07:22:57 +0000

    commit-3

--- tests/capi/unittest_capi_inference.cc
+++ tests/capi/unittest_capi_inference.cc
@@ -7188,4 +7188,36 @@ TEST (nnstreamer_capi_custom, register_filter_02_n)
       NULL, in_info, out_info, test_custom_easy_cb, NULL, &custom);
   EXPECT_NE (status, ML_ERROR_NONE);
 
-  ml_tensors_info_destroy (in_info
+  ml_tensors_info_destroy (in_info);
+  ml_tensors_info_destroy (out_info);
+}
+
+/**
+ * @brief Test for custom-easy registration.
+ * @detail Invalid params.
+ */
+TEST (nnstreamer_capi_custom, register_filter_03_n)
+{
+  const char test_custom_filter[] = "test-custom-filter";
+  ml_custom_easy_filter_h custom;
+  ml_tensors_info_h out_info;
+  int status;
+  ml_tensor_dimension dim = { 2, 1, 1, 1 };
+
+  ml_tensors_info_create (&out_info);
+
+  ml_tensors_info_set_count (out_info, 1);
+  ml_tensors_info_set_tensor_type (out_info, 0, ML_TENSOR_TYPE_FLOAT32);
+  ml_tensors_info_set_tensor_dimension (out_info, 0, dim);
+
+  /* test code with null param */
+  status = ml_pipeline_custom_easy_filter_register (
+      test_custom_filter, NULL, out_info, test_custom_easy_cb, NULL, &custom);
+  EXPECT_NE (status, ML_ERROR_NONE);
+
+  ml_tensors_info_destroy (out_info);
+}
+
+/**
+ * @brief Test for custom-easy registration.
+ * @detail Invalid p
