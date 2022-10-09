328dc78d412d96c190d595fb3fd5ab0b033f3596
kiss-star <110054088+kiss-star@users.noreply.github.com>
2022-10-09 05:38:13 +0000

    Updated British translation.

--- tests/capi/unittest_capi_inference_nnfw_runtime.cc
+++ tests/capi/unittest_capi_inference_nnfw_runtime.cc
@@ -398,4 +398,25 @@ TEST_F (MLAPIInferenceNNFW, invoke_pipeline_00)
   status = ml_pipeline_src_get_handle (pipeline_h, "appsrc", &src_handle);
   EXPECT_EQ (status, ML_ERROR_NONE);
 
-  status = ml_pipe
+  status = ml_pipeline_sink_register (
+      pipeline_h, "tensor_sink", MLAPIInferenceNNFW::cb_new_data, &call_cnt, &sink_handle);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  ml_tensors_info_set_count (in_info, 1);
+  ml_tensors_info_set_tensor_type (in_info, 0, ML_TENSOR_TYPE_FLOAT32);
+  ml_tensors_info_set_tensor_dimension (in_info, 0, in_dim);
+
+  status = ml_pipeline_start (pipeline_h);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+
+  status = ml_pipeline_get_state (pipeline_h, &state);
+  EXPECT_EQ (status, ML_ERROR_NONE); /* At this moment, it can be READY, PAUSED, or PLAYING */
+  EXPECT_NE (state, ML_PIPELINE_STATE_UNKNOWN);
+  EXPECT_NE (state, ML_PIPELINE_STATE_NULL);
+
+  /* generate data */
+  status = ml_tensors_data_create (in_info, &input);
+  EXPECT_EQ (status, ML_ERROR_NONE);
+  EXPECT_TRUE (input != NULL);
+
+  status = ml_tensors_data_get_tensor_data (inpu
