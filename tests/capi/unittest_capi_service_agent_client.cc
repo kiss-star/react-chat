a4c83b9ce6149ab4925e8fac306e5c3d64a392c1
kiss-star <110054088+kiss-star@users.noreply.github.com>
2022-11-27 19:35:02 +0000

    fix for msvc

--- tests/capi/unittest_capi_service_agent_client.cc
+++ tests/capi/unittest_capi_service_agent_client.cc
@@ -12,4 +12,47 @@
 #include <ml-api-internal.h>
 #include <ml-api-service.h>
 #include <ml-api-service-private.h>
-#include <ml-api-inference-pipeline-internal
+#include <ml-api-inference-pipeline-internal.h>
+
+#include <netinet/tcp.h>
+#include <netinet/in.h>
+
+/**
+ * @brief Test base class for Database of ML Service API.
+ */
+class MLServiceAgentTest : public::testing::Test
+{
+protected:
+  GTestDBus *dbus;
+
+public:
+  /**
+   * @brief Setup method for each test case.
+   */
+  void SetUp () override
+  {
+    gchar *current_dir = g_get_current_dir ();
+    gchar *services_dir = g_build_filename (current_dir, "tests/services", NULL);
+    dbus = g_test_dbus_new (G_TEST_DBUS_NONE);
+    ASSERT_NE (nullptr, dbus);
+
+    g_test_dbus_add_service_dir (dbus, services_dir);
+    g_free (current_dir);
+    g_free (services_dir);
+
+    g_test_dbus_up (dbus);
+  }
+
+  /**
+   * @brief Teardown method for each test case.
+   */
+  void TearDown () override
+  {
+    if (dbus) {
+      g_test_dbus_down (dbus);
+      g_object_unref (dbus);
+    }
+  }
+
+  /**
+   * @brief Get avai
